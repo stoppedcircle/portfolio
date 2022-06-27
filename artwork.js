8709f26ef1e99467223bbdc955e4b5d95af5e45b
stopped.circle <100474963+stoppedcircle@users.noreply.github.com>
2022-06-27 12:14:17 +0900

    Add files via upload

---
+++ artwork.js
@@ -0,0 +1,17 @@
+jQuery(function ($) {
+    $.fn.hScroll = function (amount) {
+        amount = amount || 120;
+        $(this).bind("DOMMouseScroll mousewheel", function (event) {
+            var oEvent = event.originalEvent, 
+                direction = oEvent.detail ? oEvent.detail * -amount : oEvent.wheelDelta, 
+                position = $(this).scrollLeft();
+            position += direction > 0 ? -amount : amount;
+            $(this).scrollLeft(position);
+            event.preventDefault();
+        })
+    };
+});
+
+$(document).ready(function() {
+    $('.shell').hScroll(60); // You can pass (optionally) scrolling amount
+});
